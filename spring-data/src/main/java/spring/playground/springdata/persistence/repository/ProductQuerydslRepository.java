f2da7b9d474a017bac061176a9c9342d6989b941
keemsy <phzzz@naver.com>
2023-12-04 23:02:45 +0900

    Update: ProductQuerydslRepository 내 누락된 파라미터 추가

--- spring-data/src/main/java/spring/playground/springdata/persistence/repository/ProductQuerydslRepository.java
+++ spring-data/src/main/java/spring/playground/springdata/persistence/repository/ProductQuerydslRepository.java
@@ -1,5 +1,7 @@
 package spring.playground.springdata.persistence.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import spring.playground.springdata.persistence.entity.ProductEntity;
 
 import java.util.List;
@@ -7,4 +9,5 @@ import java.util.List;
 
 public interface ProductQuerydslRepository {
     List<ProductEntity> findProductsByStockStatusAndPrice(String stockStatus, Integer minPrice, Integer madPrice);
+    Page<ProductEntity> findProductsByStockStatusAndPricePage(String stockStatus, Integer minPrice, Integer madPrice, Pageable pageable);
 }
