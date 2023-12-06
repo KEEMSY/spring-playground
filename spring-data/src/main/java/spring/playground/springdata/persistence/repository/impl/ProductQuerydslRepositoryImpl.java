c445b40b65abf4a1526647f9a52fca6c7c1e5cbd
keemsy <phzzz@naver.com>
2023-12-06 19:59:09 +0900

    Implement: ProductQuerydslRepositoryImpl 클래스 내 메서드 구현 - findProductsByStockStatusAndPricePage() 메서드 구현 - 관련 테스트 추가

--- spring-data/src/main/java/spring/playground/springdata/persistence/repository/impl/ProductQuerydslRepositoryImpl.java
+++ spring-data/src/main/java/spring/playground/springdata/persistence/repository/impl/ProductQuerydslRepositoryImpl.java
@@ -3,6 +3,9 @@ package spring.playground.springdata.persistence.repository.impl;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import spring.playground.springdata.persistence.entity.ProductEntity;
 import spring.playground.springdata.persistence.repository.ProductQuerydslRepository;
@@ -30,6 +33,31 @@ public class ProductQuerydslRepositoryImpl implements ProductQuerydslRepository 
                         stockStatus(stockStatus)
                 )
             .fetch();
+    }
+
+    @Override
+    public Page<ProductEntity> findProductsByStockStatusAndPricePage(String stockStatus, Integer minPrice, Integer maxPrice, Pageable pageable) {
+        List<ProductEntity> content = jpaQueryFactory
+                .select(productEntity)
+                .from(productEntity)
+                .where(
+                    priceBetween(minPrice, maxPrice),
+                    stockStatus(stockStatus)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long count = jpaQueryFactory
+                .select(productEntity.count())
+                .from(productEntity)
+                .where(
+                        priceBetween(minPrice, maxPrice),
+                        stockStatus(stockStatus)
+                )
+                .fetchOne();
+
+        return new PageImpl<>(content, pageable, count);
     }
 
     private Predicate stockStatus(String stockStatus) {
--- spring-data/src/test/java/spring/playground/springdata/persistence/repository/impl/ProductQuerydslRepositoryImplTest.java
+++ spring-data/src/test/java/spring/playground/springdata/persistence/repository/impl/ProductQuerydslRepositoryImplTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import spring.playground.springdata.persistence.entity.ProductEntity;
 import spring.playground.springdata.persistence.repository.ProductJpaRepository;
 import spring.playground.springdata.persistence.repository.ProductQuerydslRepository;
@@ -62,12 +64,62 @@ class ProductQuerydslRepositoryImplTest {
         List<ProductEntity> productEntityList = productQuerydslRepository
                 .findProductsByStockStatusAndPrice(stockStatus, minPrice, maxPrice);
 
-        System.out.println(productJpaRepository.findAll());
-
 
         // then
         assertThat(productEntityList).isNotEmpty();
         assertThat(productEntityList.size()).isEqualTo(1);
+    }
+
+    @Test
+    void findProductsByStockStatusAndPricePageTest() {
+        // given
+        List<ProductEntity> productList = new ArrayList<>();
+        ProductEntity soldoutProductEntity = ProductEntity.builder()
+                .name("testName")
+                .price(10)
+                .stock(0)
+                .build();
+
+        ProductEntity InStockProductEntity1 = ProductEntity.builder()
+                .name("testName")
+                .price(10)
+                .stock(10)
+                .build();
+
+        ProductEntity InStockProductEntity2 = ProductEntity.builder()
+                .name("testName")
+                .price(100)
+                .stock(10)
+                .build();
+
+        String stockStatus = "IN-STOCK";
+        Integer minPrice = 1;
+        Integer maxPrice = 10;
+
+        productList.add(soldoutProductEntity);
+        productList.add(InStockProductEntity1);
+        productList.add(InStockProductEntity2);
+
+        productJpaRepository.saveAll(productList);
+
+        int page = 0;
+        int size = 10;
+        PageRequest pageable = PageRequest.of(page, size);
+
+        // when
+        Page<ProductEntity> productEntityList = productQuerydslRepository
+                .findProductsByStockStatusAndPricePage(stockStatus, minPrice, maxPrice, pageable);
+
+
+        // then
+        assertThat(productEntityList).isNotEmpty();
+        assertThat(productEntityList.getContent()).hasSize(1); // Assuming two products match the criteria
+
+        // You can further assert specific properties of the returned products if needed
+        for (ProductEntity productEntity : productEntityList.getContent()) {
+            assertThat(productEntity.getStock()).isGreaterThan(0);
+            assertThat(productEntity.getPrice()).isBetween(minPrice, maxPrice);
+        }
     }
 
 }
