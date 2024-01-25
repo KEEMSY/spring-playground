109cb1a7c34721b11b70fc90f72efbd9884279c2
keemsy <phzzz@naver.com>
2024-01-25 21:37:33 +0900

    #Spring-data Update: MemberJpaRepository 메서드 추가 - Name 속성을 통한 조회 기능 추가

--- spring-data/src/main/java/spring/playground/springdata/persistence/repository/MemberJpaRepository.java
+++ spring-data/src/main/java/spring/playground/springdata/persistence/repository/MemberJpaRepository.java
@@ -3,5 +3,8 @@ package spring.playground.springdata.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring.playground.springdata.persistence.entity.member.Member;
 
+import java.util.List;
+
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
+    List<Member> findByName(String name);
 }
