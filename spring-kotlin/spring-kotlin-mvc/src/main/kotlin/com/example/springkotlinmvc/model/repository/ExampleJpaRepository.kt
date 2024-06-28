cad6abc816728dbcda51d89312a7bc28214a1d81
keemsy <phzzz@naver.com>
2024-06-28 23:46:42 +0900

    #srping-kotlin-mvc Model 계층 내 Repository 추가 - ExampleJpaRepository 추가

---
+++ spring-kotlin/spring-kotlin-mvc/src/main/kotlin/com/example/springkotlinmvc/model/repository/ExampleJpaRepository.kt
@@ -0,0 +1,9 @@
+package com.example.springkotlinmvc.model.repository
+
+import com.example.springkotlinmvc.model.entity.ExampleEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ExampleJpaRepository: JpaRepository<ExampleEntity, Long> {
+}
