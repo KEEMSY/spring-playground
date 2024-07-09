a91b4d22a73270a6af96ee8057b2ebebe8b8e4a4
keemsy <phzzz@naver.com>
2024-07-09 22:16:09 +0900

    #srping-kotlin-sync ExampleGroupQuerydslRepositoryTest 추가 - ExampleGroup id로 조회 테스트

--- spring-kotlin-sync/src/test/kotlin/com/example/kotlin/model/repository/ExampleGroupQuerydslRepositoryTest.kt
+++ spring-kotlin-sync/src/test/kotlin/com/example/kotlin/model/repository/ExampleGroupQuerydslRepositoryTest.kt
@@ -1,5 +1,6 @@
 package com.example.kotlin.model.repository
 
+import com.example.kotlin.dto.ExampleGroupOneToManySearch
 import com.example.kotlin.model.entity.ExampleEntity
 import com.example.kotlin.model.entity.ExampleGroupOneToMany
 import org.junit.jupiter.api.AfterEach
@@ -47,4 +48,27 @@ class ExampleGroupQuerydslRepositoryTest @Autowired constructor(
         assertEquals("name", actualExampleGroup.name, "Example group name should be 'name'")
         assertEquals(3, actualExampleGroup.examples.size, "Example group should have 3 examples")
     }
+
+    @Test
+    @Transactional
+    fun `ExampleGroup id로 조회 테스트`() {
+        // given
+        val exampleEntityList = mutableListOf(
+            ExampleEntity(title = "title", description = "description"),
+            ExampleEntity(title = "similarTitle", description = "similarDescription"),
+            ExampleEntity(title = "unexpectedTitle", description = "unexpectedDescription")
+        )
+        exampleJpaRepository.saveAll(exampleEntityList)
+
+        val exampleGroup = ExampleGroupOneToMany(name = "name", examples = exampleEntityList)
+        exampleGroupJpaRepository.save(exampleGroup)
+
+        // when
+        val result = exampleGroup.id?.let { exampleGroupQuerydslRepository.readExampleGroupById(it) }
+
+        // then
+        assertNotNull(result, "Result should not be null")
+        assertEquals(exampleGroup.id, result?.id, "Result id should be equal to example group id")
+    }
+
 }
