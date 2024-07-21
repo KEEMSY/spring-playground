4352c1941f5ee3faaf8ea36f983c5853c018fca3
keemsy <phzzz@naver.com>
2024-07-21 16:56:00 +0900

    #spring-kotlin-sync Service 클래스 추가 - 테스트는 사전에 작성된 컴포넌트의 테스트로 대체

---
+++ spring-kotlin-sync/src/main/kotlin/com/example/kotlin/business/ExampleService.kt
@@ -0,0 +1,39 @@
+package com.example.kotlin.business
+
+import com.example.kotlin.business.domain.Example
+import com.example.kotlin.business.port.repository.ExampleRepository
+import com.example.kotlin.dto.ExampleSearch
+import org.springframework.stereotype.Service
+
+@Service
+class ExampleService (private val exampleRepository: ExampleRepository){
+    /**
+     * Service
+     * 작성하는 메서드 명은 비즈니스 로직에 맞춰 작성한다.
+     * - 비즈니스 로직을 포함한다.
+     * - 비즈니스 이해 관계자들이 이해할 수 있는 용어를 사용한다.
+     * - DB 동작(CRUD)를 포함하지 않는다.
+     *
+     * 추후 추가할 사항
+     * - Logger
+     */
+    fun save(example: Example): Example {
+        return exampleRepository.create(example)
+    }
+
+    fun getByCriteria(exampleSearch: ExampleSearch): List<Example> {
+        return exampleRepository.readExampleListByCriteria(exampleSearch)
+    }
+
+    fun getById(id: Long): Example? {
+        return exampleRepository.readExampleById(id)
+    }
+
+    fun modify(exampleId: Long, example: Example): Example {
+        return exampleRepository.update(exampleId, example)
+    }
+
+    fun remove(id: Long) {
+        exampleRepository.delete(id)
+    }
+}
