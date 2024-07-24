017ece116d5f76d04cc96c7922579e1689387669
keemsy <phzzz@naver.com>
2024-07-24 22:45:29 +0900

    #spring-kotlin-async util 추가 - toEntity(), toDomain() 익스텐션 메서드 추가

---
+++ spring-kotlin-async/src/main/kotlin/com/example/kotlin/async/util/ExampleExtentions.kt
@@ -0,0 +1,20 @@
+package com.example.kotlin.async.util
+
+import com.example.kotlin.async.business.domain.Example
+import com.example.kotlin.async.model.entity.ExampleEntity
+
+fun Example.toEntity(): ExampleEntity {
+    return ExampleEntity(
+        id = id,
+        title = title ?: throw IllegalArgumentException("제목은 null이 될 수 없습니다."),
+        description = description ?: throw IllegalArgumentException("설명은 null이 될 수 없습니다.")
+    )
+}
+
+fun ExampleEntity.toDomain(): Example {
+    return Example(
+        id = id,
+        title = title,
+        description = description
+    )
+}
