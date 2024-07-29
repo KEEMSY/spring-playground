7010f9a089d175745511323e1dd8e6c60682420f
keemsy <phzzz@naver.com>
2024-07-29 22:49:24 +0900

    #spring-kotlin-sync Swagger 사용을 위한 코드 수정 - 누락된 build.gradle.kts 종속성 업데이트   - 카테고리 분류 주석 추가 - application.yml 내 설명 주석 추가

--- spring-kotlin-sync/build.gradle.kts
+++ spring-kotlin-sync/build.gradle.kts
@@ -27,17 +27,23 @@ repositories {
 }
 
 dependencies {
-	// coroutine
+	// Coroutine
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
 	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 
+	// Spring Boot Starters
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 
-	// querydsl
+	// Jackson and Kotlin
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// QueryDSL
 	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
 	implementation("com.querydsl:querydsl-apt:5.0.0:jakarta")
 	implementation("jakarta.persistence:jakarta.persistence-api")
@@ -45,18 +51,21 @@ dependencies {
 	kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
 	kapt("org.springframework.boot:spring-boot-configuration-processor")
 
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-
+	// Lombok
 	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-
-	testRuntimeOnly("com.h2database:h2")
-	runtimeOnly("org.postgresql:postgresql")
-
 	annotationProcessor("org.projectlombok:lombok")
 
+	// SpringDoc OpenAPI
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+
+	// DevTools
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	// Database Drivers
+	runtimeOnly("org.postgresql:postgresql")
+	testRuntimeOnly("com.h2database:h2")
+
+	// Testing
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
