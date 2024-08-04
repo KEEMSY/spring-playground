c0c5a80fabfee8b480c0e2ca8a48ed4774b99c1d
keemsy <phzzz@naver.com>
2024-08-04 21:43:10 +0900

    #spring-kotlin-user presentation 계층 내용 추가 - MemberApiController 추가

---
+++ spring-kotlin-user/src/main/kotlin/com/example/kotlin/user/presentation/MemberApiController.kt
@@ -0,0 +1,33 @@
+package com.example.kotlin.user.presentation
+
+import com.example.kotlin.user.business.MemberService
+import com.example.kotlin.user.business.domain.Member
+import com.example.kotlin.user.dto.MemberDtoRequest
+import com.example.kotlin.user.dto.MemberDtoResponse
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/api/members")
+@RestController
+class MemberApiController(
+    private val memberService: MemberService
+) {
+    /**
+     * 회원 가입
+     * ResponseEntity 으로 대체해야 함
+     */
+    @PostMapping("/sign-up")
+    fun signUp(@RequestBody memberDtoRequest: MemberDtoRequest): MemberDtoResponse {
+        val member: Member =  memberService.signUp(memberDtoRequest=memberDtoRequest)
+        return MemberDtoResponse(
+            id = member.id!!,
+            loginId = member.loginId,
+            name = member.name,
+            birthDate = member.birthDate,
+            gender = member.gender,
+            email = member.email,
+        )
+    }
+}
