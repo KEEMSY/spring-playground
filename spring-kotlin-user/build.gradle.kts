d78ee8febd0d405f640e648310eb2ebcfda55b1d
keemsy <phzzz@naver.com>
2024-08-11 23:14:33 +0900

    #spring-kotlin-user 인증과 인과를 위한 패키지 코드 추가 - Spring Security, JWT 사용을 위한 의존성 추가   - jwt secretKey 추가

--- spring-kotlin-user/build.gradle.kts
+++ spring-kotlin-user/build.gradle.kts
@@ -36,6 +36,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
+	// Security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	// JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
 	// Jackson and Kotlin
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
