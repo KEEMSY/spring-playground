27c0b03f9405b542a8edc445ee0c8119fed2d699
keemsy <phzzz@naver.com>
2024-08-24 12:17:58 +0900

    #spring-kotlin-user ResultCode 내 속성 추가 - 추가된 속성   - FORBIDDEN("권한이 없습니다.")   - NOT_FOUND("리소스를 찾을 수 없습니다.")

--- spring-kotlin-user/src/main/kotlin/com/example/kotlin/user/common/status/ResultCode.kt
+++ spring-kotlin-user/src/main/kotlin/com/example/kotlin/user/common/status/ResultCode.kt
@@ -2,5 +2,7 @@ package com.example.kotlin.user.common.status
 
 enum class ResultCode(val msg: String) {
     SUCCESS("정상 처리 되었습니다."),
-    ERROR("에러가 발생했습니다.")
+    ERROR("에러가 발생했습니다."),
+    FORBIDDEN("권한이 없습니다."),
+    NOT_FOUND("리소스를 찾을 수 없습니다."),
 }
