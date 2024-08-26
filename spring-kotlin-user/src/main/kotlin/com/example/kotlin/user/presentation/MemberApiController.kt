ce7796441172e5d71424ad3d7a9c38087904a3e3
keemsy <phzzz@naver.com>
2024-08-26 20:31:01 +0900

    #spring-kotlin-user MemberApiController 내 엔드포인트 추가 - 내 정보를 조회하는 엔드포인트 추가   - info1: 문제가 되는 엔드포인트(모든 유저의 정보를 조회할 수 있음)   - info2: 자신의 정보만 조회할 수 있음(데이터의 중복 문제)   - info3: 자신의 정보만 조회 함(accessToken을 활용)

--- spring-kotlin-user/src/main/kotlin/com/example/kotlin/user/presentation/MemberApiController.kt
+++ spring-kotlin-user/src/main/kotlin/com/example/kotlin/user/presentation/MemberApiController.kt
@@ -4,15 +4,15 @@ import com.example.kotlin.user.business.MemberService
 import com.example.kotlin.user.business.domain.Member
 import com.example.kotlin.user.common.authority.TokenInfo
 import com.example.kotlin.user.common.dto.BaseResponse
+import com.example.kotlin.user.common.dto.CustomUser
 import com.example.kotlin.user.common.util.toDtoResponse
 import com.example.kotlin.user.dto.LoginDto
 import com.example.kotlin.user.dto.MemberDtoRequest
 import com.example.kotlin.user.dto.MemberDtoResponse
 import jakarta.validation.Valid
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.annotation.*
+import org.springframework.security.access.AccessDeniedException
 
 @RequestMapping("/api/members")
 @RestController
@@ -24,13 +24,13 @@ class MemberApiController(
      */
     @PostMapping("/v1/sign-up")
     fun signUpV1(@RequestBody @Valid memberDtoRequest: MemberDtoRequest): MemberDtoResponse {
-        val member: Member =  memberService.signUp(memberDtoRequest=memberDtoRequest)
+        val member: Member = memberService.signUp(memberDtoRequest = memberDtoRequest)
         return member.toDtoResponse()
     }
 
     @PostMapping("/v2/sign-up/")
     fun signUpV2(@RequestBody @Valid memberDtoRequest: MemberDtoRequest): BaseResponse<MemberDtoResponse> {
-        val member: Member =  memberService.signUp(memberDtoRequest=memberDtoRequest)
+        val member: Member = memberService.signUp(memberDtoRequest = memberDtoRequest)
         return BaseResponse(
             message = "회원 가입 성공",
             data = member.toDtoResponse()
@@ -44,5 +44,50 @@ class MemberApiController(
     fun login(@RequestBody @Valid loginDto: LoginDto): BaseResponse<TokenInfo> {
         val tokenInfo = memberService.login(loginDto)
         return BaseResponse(data = tokenInfo)
+    }
+
+    /**
+     * 내 정보 조회하기
+     * - 문제점: 다른 유저의 정보까지 조회가 가능 함
+     */
+    @GetMapping("/info1/{id}")
+    fun searchMyInfo(@PathVariable id: Long): BaseResponse<MemberDtoResponse> {
+        val member = memberService.searchMyInfo(id)
+        return BaseResponse(data = member.toDtoResponse())
+    }
+
+    /**
+     * 내 정보 조회하기 v2
+     * - 문제점: 다른 유저의 정보까지 조회가 가능 함
+     */
+    @GetMapping("/info2/{id}")
+    fun searchMyInfoV2(@PathVariable id: Long): BaseResponse<MemberDtoResponse> {
+        val userId = (SecurityContextHolder
+            .getContext()
+            .authentication
+            .principal as CustomUser)
+            .userId
+
+        if (userId != id) {
+            throw AccessDeniedException("해당 정보에 접근할 수 없습니다.")
+        }
+        val member = memberService.searchMyInfo(id)
+        return BaseResponse(data = member.toDtoResponse())
+    }
+
+    /**
+     * 내 정보 조회하기
+     * - 로그인한 사람의 정보만 조회할 수 있도록 수정
+     */
+    @GetMapping("/info3")
+    fun searchMyInfo(): BaseResponse<MemberDtoResponse> {
+        val userId = (SecurityContextHolder
+            .getContext()
+            .authentication
+            .principal as CustomUser)
+            .userId
+        val member = memberService.searchMyInfo(userId)
+
+        return BaseResponse(data = member.toDtoResponse())
     }
 }
