c117900eae179bf14a50495bfded379633518f5e
keemsy <phzzz@naver.com>
2024-09-05 23:00:02 +0900

    #spring-user basic.js 수정 - 잘못 삭제한 "Bearer " 헤더 복구   - 간편로그인을 통한 토큰 인증 시, 해당 코드가 없어 로직에 문제가 발생(토큰 처리 과정 중 훼손)

--- spring-user/src/main/resources/static/js/basic.js
+++ spring-user/src/main/resources/static/js/basic.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
     if (auth !== undefined && auth !== '') {
         $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
-            jqXHR.setRequestHeader('Authorization', auth);
+            jqXHR.setRequestHeader('Authorization', "Bearer " + auth);
         });
     } else {
         window.location.href = host + '/api/user/login-page';
