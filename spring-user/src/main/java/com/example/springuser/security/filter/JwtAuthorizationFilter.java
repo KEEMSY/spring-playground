85f1a13f10a05989942e91fea9bd13ff79a01757
keemsy <phzzz@naver.com>
2024-09-10 22:10:35 +0900

    #spring-user JwtAuthorizationFilter 업데이트 - 정적 자원 경로를 처리하지 않도록 처리 - 로깅 코드 추가

--- spring-user/src/main/java/com/example/springuser/security/filter/JwtAuthorizationFilter.java
+++ spring-user/src/main/java/com/example/springuser/security/filter/JwtAuthorizationFilter.java
@@ -31,10 +31,22 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
         String tokenValue = jwtUtil.getJwtFromHeader(req);
-        if (StringUtils.hasText(tokenValue)) {
+        String requestURI = req.getRequestURI();
 
+        // 로그인 페이지와 정적 자원 경로를 제외
+        if (requestURI.equals("/api/user/login-page") ||
+                requestURI.startsWith("/css/") ||
+                requestURI.startsWith("/images/") ||
+                requestURI.startsWith("/js/") ||
+                requestURI.equals("/favicon.ico")) {
+            filterChain.doFilter(req, res);
+            return;
+        }
+
+        if (StringUtils.hasText(tokenValue)) {
             if (!jwtUtil.validateToken(tokenValue)) {
-                log.error("Token Error");
+                log.error("Expired JWT token");
+                res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Expired JWT token");
                 return;
             }
 
@@ -42,8 +54,10 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
             try {
                 setAuthentication(info.getSubject());
+                log.info("success setAuthentication");
             } catch (Exception e) {
-                log.error(e.getMessage());
+                log.error("Authentication Error: " + e.getMessage());
+                res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Authentication Error");
                 return;
             }
         }
@@ -58,11 +72,13 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         context.setAuthentication(authentication);
 
         SecurityContextHolder.setContext(context);
+        log.info("setAuthentication: " + username);
     }
 
     // 인증 객체 생성
     private Authentication createAuthentication(String username) {
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        log.info("createAuthentication: " + userDetails.getUsername());
         return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 }
